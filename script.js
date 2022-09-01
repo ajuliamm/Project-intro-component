b66541779dcf5191c085055aed146eb20f4b56e6
Angelina Julia <ajuliammelo@gmail.com>
2022-08-31 21:55:57 -0300

    add versão mobile

---
+++ script.js
@@ -0,0 +1,13 @@
+const menuToggle = () =>{
+    const nav = document.getElementById('nav')
+        nav.classList.toggle('active')
+        
+        const menu2 = document.getElementById('menu-ul2')
+        menu2.parentNode.removeChild(menu2)
+
+        
+           
+        
+    
+}
+
